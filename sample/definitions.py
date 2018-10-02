4045f3ba4e6a49e0b88a0d20ced5aaeeaafd4a79
haroon <haroon@pinger.com>
2018-10-02 15:18:59 -0700

    Adding path variables for images

---
+++ sample/definitions.py
@@ -0,0 +1,4 @@
+import os
+
+ROOT_DIR = os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(__file__)), os.pardir))
+IMAGE_FOLDER_PATH = os.path.join(ROOT_DIR, 'resources')
--- sample/fileHelpers.py
+++ sample/fileHelpers.py
@@ -1,20 +1,22 @@
 import os
+from definitions import *
 
 #function to return list of persons from a directory
 def getPersons(dirName):
-    fullDirName = os.path.dirname(os.path.realpath(__file__)) + '/../src/' + dirName
+    fullDirName = IMAGE_FOLDER_PATH + '/' + dirName
     print(fullDirName)
     persons = [name for name in os.listdir(fullDirName)
             if os.path.isdir(os.path.join(fullDirName, name))]
     return persons
     
-#getPersons('Students')
+#print(getPersons('Students'))
 
 # function to return list of pictures of a person from a directory 
 def getPersonPictures(friendName, folderName):
-    fullDirName = os.path.dirname(os.path.realpath(__file__)) + '/../src/' + folderName + '/' + friendName
+    fullDirName = IMAGE_FOLDER_PATH + '/' + folderName + '/' + friendName
     #personPictures = [file for file in os.listdir(fullDirName) if file.endswith(".jpg") or file.endswith(".jpeg")]
-    personPictures = [fullDirName + '/' + file for file in os.listdir(fullDirName) if file.endswith(".jpg") or file.endswith(".jpeg")]
+    personPictures = [fullDirName + '/' + file 
+        for file in os.listdir(fullDirName) if file.endswith(".jpg") or file.endswith(".jpeg")]
     #print(personPictures)
     return personPictures
 
