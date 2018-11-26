34b2bc3b676ec7375c05b5a616e26463f8ebc9b6
kedharanethra <kedharanethra.thiruvuru@sjsu.edu>
2018-11-25 21:17:43 -0800

    Image capture and curl scripts

---
+++ rs_scripts/Autocap.py
@@ -0,0 +1,8 @@
+import picamera     # Importing the library for camera module
+from time import sleep
+camera = picamera.PiCamera()    # Setting up the camera
+camera.start_preview()
+sleep(5)
+camera.capture('/home/pi/Imagecap/imag.jpg') # Capturing the image
+camera.stop_preview()
+print('Done')
---
+++ rs_scripts/criminals.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+DATE=$(date +"%Y-%m-%d_%H%M%S")
+#raspistill -o /home/pi/timelapse/$DATE.jpg
+#raspistill -o /mnt/picam/$DATE.jpg -tl 0 -t 60000
+raspistill -o $(pwd)/images/$DATE%01d.jpg -t 20000 -tl 10000
+for imagenumber in {0..2}
+do
+imageUrl=$(pwd)"/images/"$DATE$imagenumber".jpg"
+echo $imageUrl
+data="{"\"imageUrl\"" : "\"$imageUrl\"", "\"personGroupId\"" : "\"criminals\""}"
+curl --header "Content-Type: application/json" --request POST --data "$data" http://localhost:9999/v1/detect
+done
